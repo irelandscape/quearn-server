3294e50a058dfeb62d2234f74676fd26a987ce09
Olivier Deme <olivier.deme@gmail.com>
2018-12-28 10:11:17 +0000

    Merge from RELEASE-0.1.2

--- quearn_server/quearn_server/settings.py
+++ quearn_server/quearn_server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_h+3c$xvv!ddi3jgb6yb1^p0t11_w*%f2#s$g&=g*cqg)kluh3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['www.stemq.io',
   'api.stemq.io',
